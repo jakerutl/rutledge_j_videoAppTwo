7ad9c7354735ff8c4953db21b39df1047071fa1a
jakerutl <j_rutledge3@fanshaweonline.ca>
2018-04-20 21:51:53 -0400

    styling some things

--- controllers/videoAppController.js
+++ controllers/videoAppController.js
@@ -80,6 +80,26 @@ exports.post_new_review = (req, res) => {
   })
 };
 
+exports.post_new_review = (req, res) => {
+  console.log('hit post review route');
+
+  connect.getConnection((err, connection) => {
+    if (err) {
+      return console.log(err.message);
+    }
+
+    query = `INSERT INTO tbl_comments (comments_id, comments_auth, comments_copy, comments_date, comments_movie, comments_rating) VALUES (NULL, NULL, "${req.body.comment}", CURRENT_TIMESTAMP(), "${req.body.id}", "${req.body.stars}");`;
+
+    connect.query(query, (err, rows) => {
+      if (err) {
+        return console.log(err.message);
+      }
+
+      res.json(rows);
+    })
+  })
+};
+
 
 // router.post('/', function(req, res) {
 //   console.log('post a review');
